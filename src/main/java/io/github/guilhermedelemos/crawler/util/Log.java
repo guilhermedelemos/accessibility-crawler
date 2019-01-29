b696c8990c271284ddb419c07fcf4337fb72101c
Guilherme de Lemos <guilherme.eti@gmail.com>
2019-01-29 01:03:17 -0200

    Code Smells

--- src/main/java/io/github/guilhermedelemos/crawler/util/Log.java
+++ src/main/java/io/github/guilhermedelemos/crawler/util/Log.java
@@ -4,15 +4,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Log {
+
+    private Log() {}
+
     public static void log(String message) {
         log(message, true);
     }
+
     public static void log(String message, boolean date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         StringBuilder sb = new StringBuilder();
-        sb.append(dateFormat.format(new Date()));
-        sb.append(": ");
+        if(date) {
+            sb.append(dateFormat.format(new Date()));
+            sb.append(" ");
+        }
         sb.append(message);
         System.out.println(sb.toString());
     }
+
 }
