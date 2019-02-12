ac9ec3d1f6ee3d1613b396e79b7eb67739f6890f
Guilherme de Lemos <guilherme.eti@gmail.com>
2019-02-12 17:28:06 -0200

    Fix Log

--- src/main/java/io/github/guilhermedelemos/crawler/util/Log.java
+++ src/main/java/io/github/guilhermedelemos/crawler/util/Log.java
@@ -8,6 +8,8 @@ public class Log {
 
     private static final String SEPARATOR = "--";
     private static final String ARROW = "> ";
+    public static final String LOG_PARAMS2 = "%s%s";
+    public static final String LOG_PARAMS3 = "%s%s%s";
 
     private Log() {}
 
@@ -24,33 +26,33 @@ public class Log {
             indentation.append(ARROW);
         }
         log.info("-----");
-        log.info(String.format("%s%s%s", indentation, "id", element.getAttribute("id")));
-        log.info(String.format("%s%s%s", indentation, "TagName ", element.getTagName()));
-        log.info(String.format("%s%s%s", indentation, "Location X ", element.getLocation().getX()));
-        log.info(String.format("%s%s%s", indentation, "Location Y ", element.getLocation().getY()));
-        log.info(String.format("%s%s%s", indentation, "Height ", element.getSize().getHeight()));
-        log.info(String.format("%s%s%s", indentation, "Width ", element.getSize().getWidth()));
-        log.info(String.format("%s%s%s", indentation, "Displayed ", element.isDisplayed()));
-        log.info(String.format("%s%s%s", indentation, "Enabled ", element.isEnabled()));
-        log.info(String.format("%s%s%s", indentation, "Selected ", element.isSelected()));
-        log.info(String.format("%s%s%s", indentation, "Text ", element.getText()));
+        log.info(String.format(LOG_PARAMS3, indentation, "id", element.getAttribute("id")));
+        log.info(String.format(LOG_PARAMS3, indentation, "TagName ", element.getTagName()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Location X ", element.getLocation().getX()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Location Y ", element.getLocation().getY()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Height ", element.getSize().getHeight()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Width ", element.getSize().getWidth()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Displayed ", element.isDisplayed()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Enabled ", element.isEnabled()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Selected ", element.isSelected()));
+        log.info(String.format(LOG_PARAMS3, indentation, "Text ", element.getText()));
     }
 
-    public void logWebPage(WebPage webPage) {
+    public static void logWebPage(WebPage webPage, Logger log) {
         if(webPage == null) {
             return;
         }
         webPage.getElements().forEach(element -> {
             log.info(Messages.LOG_ELEMENT_FOUND);
-            log.info(String.format(LOG_PARAMS, Messages.LOG_ID, element.getId()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_TAG_NAME, element.getTagName()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_POSITION_X, element.getX()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_POSITION_Y, element.getY()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_HEIGHT, element.getHeight()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_WIDTH, element.getWidth()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_DISPLAYED, element.isDisplayed()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_ENABLED, element.isEnabled()));
-            log.info(String.format(LOG_PARAMS, Messages.LOG_TEXT, element.getText()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_ID, element.getId()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_TAG_NAME, element.getTagName()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_POSITION_X, element.getX()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_POSITION_Y, element.getY()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_HEIGHT, element.getHeight()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_WIDTH, element.getWidth()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_DISPLAYED, element.isDisplayed()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_ENABLED, element.isEnabled()));
+            log.info(String.format(LOG_PARAMS2, Messages.LOG_TEXT, element.getText()));
         });
     }
 
